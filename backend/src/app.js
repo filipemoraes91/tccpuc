b766e101be9a100f213616508bd19c63bb278fef
filipemoraes91 <58920399+filipemoraes91@users.noreply.github.com>
2024-05-31 17:39:12 -0300

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,12 @@ const cors = require('cors');
 
 const router = require('./router');
 
+const corsOptions = {
+     origin: 'https://tccbuild-filipepmoraes91s-projects.vercel.app/', // Substitua pelo URL do seu frontend
+     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+     allowedHeaders: ['Content-Type', 'Authorization'],
+     credentials: true, // Se você precisa passar cookies ou autenticação
+};
 
 const app = express();
 app.use(express.json());
